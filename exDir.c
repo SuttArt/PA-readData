304397ee8ce90f1080fb5b9b03ed4a5b70f97f33
Artem Suttar <artem.suttar@gmail.com>
2021-11-25 11:45:21 +0100

    25.11.2021

--- exDir.c
+++ exDir.c
@@ -5,11 +5,7 @@ int main(void)
     DIR* dir;
     char x [80]; //string in C => char* x; => dann malloc() benutzen
     struct dirent* d_pntr;
-    char name_in[80]; // full name of source name
-    char name_out[80]; //full name of target file
-    FILE* in; // handler
-    FILE* out;
-    char ch;
+
 
     dir = opendir("./A");
     if(dir != NULL)
@@ -18,28 +14,101 @@ int main(void)
         {
             //strcpy(x,d_pntr -> d_name);
             strcpy(x,(*d_pntr).d_name);
-            printf("\n%s",x);
-            strcpy(name_in, "./A/");
-            strcpy(name_out, "./B/");
-            strcat(name_in, x); // "./A/dat01.txt"
-            strcat(name_out, x);
-            in = fopen(name_in,"rb"); //rb ?! Byte? Binary??
-            out = fopen(name_out,"wb");
-
-            while ((ch=fgetc(in)) != EOF) //EOF - End Of File
+            printf("\nName: %s",x);
+            if (strcmp(x, ".") == 1 && strcmp(x,"..") == 1) //(x[0] != '.' || (x[0] != '.' && x[1] != '.'))
             {
-                fputc(ch,out);
+                makeList(x);
             }
-//            while ((ch=fgetc(in)) != EOF) //EOF - End Of File
-//                fputc(ch,out); //Geht auch, weil wir nur 1 Anweisung hier haben
-            fclose(in);
-            fclose(out);
+
+            iterateListAndCopy();
 
         }
         closedir(dir);
+        iterateList();
+        deleteList();
     }
     return 0;
 }
 
 //TODO: 1) List mit Dateinamen aufbauen.
 //TODO: 2) Diese Liste durchgehen und Dateien von ./A nach ./B zu kopieren.
+void makeList(char* nameOfFile)
+{
+    DataList* current = NULL; //current/working Node of the List
+
+    int gDataListsize = sizeof (DataList);
+
+    current = (DataList*) malloc(gDataListsize);//Storage reservation
+    strcpy(current -> name, nameOfFile);
+    current -> mutex = 0;
+    current -> threadNr = 0;
+    current -> next = NULL;
+    //list is empty
+    if (gFirstData == NULL)
+    {
+        gFirstData = current;
+    }
+    else
+    {
+        gLastData -> next = current;
+    }
+    gLastData = current;
+}
+
+void iterateList(void)
+{
+    DataList* current = gFirstData; //current/working Node of the List
+
+    while (current != NULL)
+    {
+        printf("\n== my address: %p == name of file: %s == address of next: %p",current ,current->name, current->next);
+        current = current->next;
+
+    }
+}
+
+void iterateListAndCopy(void)
+{
+    DataList* current = gFirstData; //current/working Node of the List
+    char name_in[80]; // full name of source name
+    char name_out[80]; //full name of target file
+    FILE* in; // handler
+    FILE* out;
+    char ch;
+
+    while (current != NULL)
+    {
+        strcpy(name_in, "./A/");
+        strcpy(name_out, "./B/");
+        strcat(name_in, current -> name); // "./A/dat01.txt"
+        strcat(name_out, current -> name);
+
+        in = fopen(name_in,"rb"); //rb ?! Byte? Binary??
+        out = fopen(name_out,"wb");
+
+        while ((ch=fgetc(in)) != EOF) //EOF - End Of File
+        {
+            fputc(ch,out);
+        }
+//            while ((ch=fgetc(in)) != EOF) //EOF - End Of File
+//                fputc(ch,out); //Geht auch, weil wir nur 1 Anweisung hier haben
+        fclose(in);
+        fclose(out);
+
+        current = current->next;
+    }
+}
+
+void deleteList(void)
+{
+    DataList* current = gFirstData; //current/working Node of the List
+    DataList* x = NULL; //temporary Node of the List
+
+    while (current != NULL)
+    {
+        printf("\nFreigabe: %s", current->name);
+        x = current->next;
+        free(current);
+        current = x;
+    }
+}
--- exDir.h
+++ exDir.h
@@ -3,4 +3,27 @@
 #include <dirent.h>
 
 
+//Precompiler-Variablen
 
+
+//Datatypes
+typedef struct DataList
+{
+    char name[80];
+    int mutex;
+    int threadNr;
+    struct DataList* next; //address of the next node
+}
+DataList; //this is ONE node in the List
+
+
+//Protorypen
+void makeList(char* nameOfFile);
+void iterateList(void);
+void iterateListAndCopy(void);
+void deleteList(void);
+
+//global Variables
+DataList* gFirstData = NULL;   // der 1. Knoten in der Liste
+DataList* gLastData = NULL;    // der letzte Knoten in der Liste
+int gDataListsize;   //Größe von DataList
